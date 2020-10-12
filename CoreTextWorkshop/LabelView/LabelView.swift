27d1d851821610e0dce115adf293eeb8a3a6b6a7
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-10-12 18:18:44 +0200

    Initial commit

---
+++ CoreTextWorkshop/LabelView/LabelView.swift
@@ -0,0 +1,26 @@
+import UIKit
+import CoreText
+
+/// Custom label view
+final class LabelView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    // MARK: - Draw
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+    }
+
+    // MARK: - Layout
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
