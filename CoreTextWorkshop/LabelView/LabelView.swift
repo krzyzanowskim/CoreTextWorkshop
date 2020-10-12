a7f144e188748149c40fb13df37dfe160207bcbf
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-10-12 19:13:24 +0200

    Setup empty LabelView

--- CoreTextWorkshop/LabelView/LabelView.swift
+++ CoreTextWorkshop/LabelView/LabelView.swift
@@ -2,8 +2,12 @@ import UIKit
 import CoreText
 
 /// Custom label view
-final class LabelView: UIView {
-    
+@IBDesignable
+final public class LabelView: UIView {
+
+    @IBInspectable
+    public var text: String?
+
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -12,15 +16,31 @@ final class LabelView: UIView {
         super.init(coder: coder)
     }
 
+    public override class func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+    }
+
     // MARK: - Draw
 
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
+    public override func draw(_ dirtyRect: CGRect) {
+        super.draw(dirtyRect)
+
+        // Get CGContext to draw on
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+
+        context.saveGState()
+
+        // Do the drawing here in `bounds` or `dirtyRect`
+        // START HERE
+
+        context.restoreGState()
     }
 
     // MARK: - Layout
 
-    override func layoutSubviews() {
+    public override func layoutSubviews() {
         super.layoutSubviews()
     }
 }
