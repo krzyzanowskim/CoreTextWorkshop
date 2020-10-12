df7d19a0e3cfc5c7b93526c0a7ab47faf3c28cea
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-10-13 00:29:08 +0200

    CTTypesetterSuggestLineBreak

--- CoreTextWorkshop/LabelView/LabelView.swift
+++ CoreTextWorkshop/LabelView/LabelView.swift
@@ -41,53 +41,27 @@ final public class LabelView: UIView {
         context.textMatrix = CGAffineTransform(scaleX: 1.0, y: -1.0)
 
         let attributedString = NSAttributedString(string: text, attributes: [.font : textFont])
-        let framesetter = CTFramesetterCreateWithAttributedString(attributedString)
-        let ctFrame = CTFramesetterCreateFrame(framesetter, CFRange(), CGPath(rect: bounds, transform: nil), nil)
+        let typesetter = CTTypesetterCreateWithAttributedString(attributedString)
 
-        let ctLines = CTFrameGetLines(ctFrame) as! [CTLine]
+        // LeftRight/TopBottom
+        var startCharacterIndex = 0
+        var currentY: CGFloat = 0
+        while startCharacterIndex < attributedString.length {
+            let breakCharacterIndex = startCharacterIndex + CTTypesetterSuggestLineBreak(typesetter, startCharacterIndex, Double(bounds.width))
+            let ctLine = CTTypesetterCreateLine(typesetter, CFRange(location: startCharacterIndex, length: breakCharacterIndex - startCharacterIndex))
 
-        var ctLinesOrigins = Array<CGPoint>(repeating: .zero, count: ctLines.count)
-        // Get origins in CoreGraphics coodrinates
-        CTFrameGetLineOrigins(ctFrame, CFRange(), &ctLinesOrigins)
+            var ascent: CGFloat = 0
+            var descent: CGFloat = 0
+            var leading: CGFloat = 0
+            CTLineGetTypographicBounds(ctLine, &ascent, &descent, &leading)
 
-        // Draw lines at origins
-        for (ctLine, lineOrigin) in zip(ctLines, ctLinesOrigins) {
+            let lineHeight = ascent + descent + leading
 
-            // Tranform coordinates for iOS
-            let transformedLineOrigin = lineOrigin.applying(.init(scaleX: 1, y: -1))
-                                                  .applying(.init(translationX: 0, y: bounds.height))
+            context.textPosition = CGPoint(x: 0, y: currentY + ascent)
+            CTLineDraw(ctLine, context)
 
-            context.textPosition = transformedLineOrigin
-
-            for styleRun in CTLineGetGlyphRuns(ctLine) as! [CTRun] {
-                let glyphsCount = CTRunGetGlyphCount(styleRun)
-
-                var glyphs = [CGGlyph](repeating: .zero, count: glyphsCount)
-                CTRunGetGlyphs(styleRun, CFRange(), &glyphs)
-
-                var glyphsPositions = [CGPoint](repeating: .zero, count: glyphsCount)
-                CTRunGetPositions(styleRun, CFRange(), &glyphsPositions)
-
-                let runAttributes = CTRunGetAttributes(styleRun) as! [String: Any]
-                let font = (runAttributes[kCTFontAttributeName as String] as? UIFont) ?? self.textFont
-
-                for (glyph, position) in zip(glyphs, glyphsPositions) {
-                    guard let glyphPath = CTFontCreatePathForGlyph(font, glyph, nil) else {
-                        // whitespace has no glyph
-                        continue
-                    }
-
-                    context.saveGState()
-
-                    // Adjust path position, or change local context translateBy, scaleBy
-                    var pathTransformation = CGAffineTransform(translationX: position.x, y: transformedLineOrigin.y).scaledBy(x: 1.0, y: -1.0)
-                    let tranformedPath = glyphPath.copy(using: &pathTransformation)!
-
-                    context.addPath(tranformedPath)
-                    context.fillPath()
-                    context.restoreGState()
-                }
-            }
+            startCharacterIndex = breakCharacterIndex
+            currentY += lineHeight
         }
 
         context.restoreGState()
